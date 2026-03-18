53f9c3d0261fcd39c154f43ad2a919eac45fb8ca
Sofie <sov.002@edu.zealand.dk>
2026-03-18 15:06:56 +0100

    home page and deleted json added

---
+++ app/pages/Home.jsx
@@ -0,0 +1,5 @@
+// To navigate between pages, you use the Link component
+// provided by next/link.
+// • This component enables client-side navigation to a different
+// route in the application, which is faster than the default
+// navigation done by the browser. -Nilmas slide 12
